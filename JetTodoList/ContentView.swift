2dfe102d1ba31a97923685c70e18f96ab80968f1
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-07-13 09:49:08 +0500

    Initial Commit

---
+++ JetTodoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JetTodoList
+//
+//  Created by Thor on 13/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JetTodoList/JetTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetTodoListApp.swift
+//  JetTodoList
+//
+//  Created by Thor on 13/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct JetTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
